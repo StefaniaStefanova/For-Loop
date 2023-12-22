614808b3d891109e28f56eea6775f3970c75a142
StefaniaStefanova <StefaniaStefanova@users.noreply.github.com>
2023-12-22 19:10:47 +0200

    Add project files.

---
+++ 07. Sum Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 09. Left and Right Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _09._Left_and_Right_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int leftSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                leftSum += int.Parse(Console.ReadLine());
+            }
+
+            int rightSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                rightSum += int.Parse(Console.ReadLine());
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                int diff = Math.Abs(leftSum - rightSum);
+                Console.WriteLine($"No, diff = {diff}");
+            }
+        }
+    }
+}
